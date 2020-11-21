2b90f0355a320c11615831155b516fdada1b3d96
swimmingkiim <swimmingkiim@gmail.com>
2020-11-21 16:05:46 +0900

    success download node and install it

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
+import subprocess
 
 if __name__ == '__main__':
-    os.system('echo hello')
+    # subprocess.call('curl https://nodejs.org/dist/v14.15.1/node-v14.15.1-x64.msi -o node-v14.15.1-x64.msi')
+    # print('done : download node version 14.15.1 msi file')
+    # subprocess.call('msiexec.exe /l i /passive /package node-v14.15.1-x64.msi')
+    # print('done : install node')
+    print('finished')
